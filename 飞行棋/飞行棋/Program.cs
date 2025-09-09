c100caf40009a4caf85b1dd1bec2ec9305288e06
彭彭 <pjd0331@outlook.com>
2025-09-09 17:52:32 +0800

    9.9-1

---
+++ 飞行棋/飞行棋/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace 飞行棋
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 控制台初始化
+            int w = 50;
+            int h = 30;
+            ConsoleInit(w, h);
+            #endregion 
+        }
+        static void ConsoleInit(int w, int h)
+        {
+            //窗口的大小
+            Console.SetWindowSize(w, h);
+            Console.SetBufferSize(w, h);
+            //光标的隐藏
+            Console.CursorVisible = false;
+        }
+    }
+
+}
